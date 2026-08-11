fff1fd529a59627beb097260b425d48ff14382cc
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-11 16:22:01 +0300

    Use enhanced login page for OTP and Google authentication

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,11 @@ use App\Http\Controllers\OtpController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('landing');
-})->name('landing');
+Route::get('/', function () { return view('landing'); })->name('landing');
 
 Route::middleware('guest')->group(function () {
-    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::get('/login', fn () => view('auth.login-enhanced'))->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
-    Route::get('/login/otp', fn () => view('auth.login'))->name('login.otp');
     Route::post('/login/otp/request', [OtpController::class, 'requestOtp'])->name('login.otp.request');
     Route::post('/login/otp/verify', [OtpController::class, 'verify'])->name('login.otp.verify');
     Route::get('/auth/google', [GoogleAuthController::class, 'redirect'])->name('auth.google');
@@ -25,7 +22,6 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
     Route::get('/products/analytics', [ProductController::class, 'analytics'])->name('products.analytics');
     Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
@@ -37,7 +33,6 @@ Route::middleware('auth')->group(function () {
     Route::post('/products/{product}/stock-in', [ProductController::class, 'processStockIn'])->name('products.stockIn.submit');
     Route::get('/products/{product}/stock-out', [ProductController::class, 'stockOut'])->name('products.stockOut');
     Route::post('/products/{product}/stock-out', [ProductController::class, 'processStockOut'])->name('products.stockOut.submit');
-
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
     Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
     Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
