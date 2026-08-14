ff383d8bbaa600496326d362b786be241bee35f9
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-14 16:54:05 +0300

    Phase 4: expose public MTN payment callback endpoint

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\StorefrontController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () { return view('landing'); })->name('landing');
+Route::post('/payments/callback/mtn', [PaymentController::class, 'callbackMtn'])->name('payments.callback.mtn');
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', fn () => view('auth.login-enhanced'))->name('login');
@@ -26,23 +27,19 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
     Route::get('/shop', [StorefrontController::class, 'index'])->name('storefront.index');
     Route::get('/shop/product/{product}', [StorefrontController::class, 'show'])->name('storefront.show');
     Route::post('/shop/cart/add/{product}', [StorefrontController::class, 'addToCart'])->name('storefront.cart.add');
     Route::get('/shop/cart', [StorefrontController::class, 'cart'])->name('storefront.cart');
     Route::post('/shop/cart', [StorefrontController::class, 'updateCart'])->name('storefront.cart.update');
     Route::delete('/shop/cart/{product}', [StorefrontController::class, 'removeFromCart'])->name('storefront.cart.remove');
-
     Route::get('/checkout', [CheckoutController::class, 'create'])->name('checkout');
     Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
     Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
     Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
-
     Route::get('/orders/{order}/pay', [PaymentController::class, 'create'])->name('payments.create');
     Route::post('/orders/{order}/pay', [PaymentController::class, 'store'])->name('payments.store');
     Route::get('/orders/{order}/payments/{payment}', [PaymentController::class, 'show'])->name('payments.show');
-
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
     Route::get('/products/analytics', [ProductController::class, 'analytics'])->name('products.analytics');
     Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
