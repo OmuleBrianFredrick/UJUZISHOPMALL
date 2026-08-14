ca19c47e55fabbdd4ba911b34f3c6424174d978d
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-14 16:53:47 +0300

    Phase 4: configure MTN MoMo credentials via environment

--- config/services.php
+++ config/services.php
@@ -1,45 +1,30 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
-    'postmark' => [
-        'key' => env('POSTMARK_API_KEY'),
-    ],
-
-    'resend' => [
-        'key' => env('RESEND_API_KEY'),
-    ],
-
+    'postmark' => ['key' => env('POSTMARK_API_KEY')],
+    'resend' => ['key' => env('RESEND_API_KEY')],
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-
     'africastalking' => [
         'username' => env('AFRICASTALKING_USERNAME'),
         'api_key' => env('AFRICASTALKING_API_KEY'),
         'sender_id' => env('AFRICASTALKING_SENDER_ID'),
         'endpoint' => env('AFRICASTALKING_ENDPOINT'),
     ],
-
+    'mtn_momo' => [
+        'base_url' => env('MTN_MOMO_BASE_URL', 'https://sandbox.momodeveloper.mtn.com'),
+        'subscription_key' => env('MTN_MOMO_SUBSCRIPTION_KEY'),
+        'api_user' => env('MTN_MOMO_API_USER'),
+        'api_key' => env('MTN_MOMO_API_KEY'),
+        'target_environment' => env('MTN_MOMO_TARGET_ENVIRONMENT', 'sandbox'),
+    ],
 ];
